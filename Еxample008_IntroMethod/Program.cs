fe8a6f1926f824060e1377ea10269edf5b695480
Natalia-Bogatyreva <rogova_nu@mail.ru>
2022-12-03 12:28:26 +0500

    Function

---
+++ Еxample008_IntroMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+int result = arg1;
+if(arg2>result) result=arg2;
+if(arg3>result) result=arg3;
+return result;
+}
+int a1 = 33;
+int a2 = 13;
+int a3 = 23;
+int b1 = 313;
+int b2 = 39;
+int b3 = 15;
+int c1 = 10;
+int c2 = 1;
+int c3 = 45;
+
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+Console.WriteLine(max);
