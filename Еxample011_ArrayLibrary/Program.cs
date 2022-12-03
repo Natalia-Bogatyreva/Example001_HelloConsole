3b49f21d734afa48971874e50be69ada5b846598
Natalia-Bogatyreva <rogova_nu@mail.ru>
2022-12-03 13:15:16 +0500

    Array_void

---
+++ Еxample011_ArrayLibrary/Program.cs
@@ -0,0 +1,45 @@
+﻿void FillArray(int[] coll)
+{
+int length = coll.Length;
+int index = 0;
+while (index < length)
+{
+ coll[index] = new Random().Next(1,10);
+ index++;
+}
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] coll, int find)
+{
+    int count = coll.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (coll[index] == find)
+        {
+            position = index;
+            break;
+        }
+    index++;
+    }
+return position;
+}
+
+int [] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos = IndexOf(array, 6);
+Console.WriteLine(pos);
