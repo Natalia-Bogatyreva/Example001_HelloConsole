e1042f3d8522041193e3570a2cb8a456a1785237
Natalia-Bogatyreva <rogova_nu@mail.ru>
2022-12-09 23:13:43 +0500

    Fibbonachi

---
+++ Example013_recursFact/Program.cs
@@ -0,0 +1,21 @@
+﻿// Факториал и переполнение
+/*{
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+*/
+
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"F{i} = {Fibonacci(i)}");
+}
